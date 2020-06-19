4d86ad2a401fbf7d09c79ea2b9dcaa9ac4161fe7
엔텔스 김용주 <yjkim@ntels.com>
2020-06-19 13:00:21 +0900

    first commit

---
+++ src/hash/RaceClass.java
@@ -0,0 +1,41 @@
+package hash;
+
+import java.util.*;
+
+public class RaceClass {
+    public static String raceArraysSolution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[completion.length];
+    }
+
+    public static String raceHashSolution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap();
+        for (String player : participant) map.put(player, map.getOrDefault(player, 0) + 1);
+        for (String player : completion) map.put(player, map.get(player) - 1);
+
+/*        for (String player : participant) {
+            if(map.get(player) != 0) {
+                return player;
+            }
+        }*/
+
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String arg[]) {
+        String[] participant = {"aaa", "bbb", "ccc"};
+        String[] completion = {"aaa", "bbb"};
+        String result = raceArraysSolution(participant, completion);
+    }
+}
---
+++ src/stackqueue/truck.java
@@ -0,0 +1,18 @@
+package stackqueue;
+
+import java.util.*;
+/**
+ * Truck example
+ * @author yongjoo / werbwerb@naver.com
+ */
+public class truck {
+    public static void mapSolution() {
+        String[] bridgeLength;
+        String[] truckWeight;
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
