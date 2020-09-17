b9047a8aa4c09e6e6cc36316667233997025ff1a
yjkim <werbwerb@naver.com>
2020-09-17 20:47:32 +0900

    1commit

--- src/stackqueue/Jusik.java
+++ src/stackqueue/Jusik.java
@@ -2,20 +2,32 @@ package stackqueue;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Jusik {
     public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        String prices = br.readLine();
-        String[] formatPrices = prices.split(",");
-        for (String formatPrice : formatPrices) {
-            System.out.println("RESULT >>> " + formatPrice);
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] answer = {};
+        answer = new int[prices.length];
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int price : prices) {
+            arrayList.add(price);
         }
-    }
 
-    public void solution() {
-
+        for (int i = 0; i < arrayList.size(); i++) {
+            int cnt = 0;
+            for (int j = i + 1; j < arrayList.size(); j++) {
+                if(arrayList.get(i) <= arrayList.get(j)) {
+                    ++cnt;
+                } else {
+                    ++cnt;
+                    break;
+                }
+            }
+            answer[i] = cnt;
+            System.out.println(answer[i]);
+        }
     }
 }
